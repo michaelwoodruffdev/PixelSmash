06b0d526a1fac5c9d8ec95f464d8d873f5576a01
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-04-20 13:06:37 -0500

    basic layout for lobby

--- frontend/src/layouts/dashboard/Dashboard.js
+++ frontend/src/layouts/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Dashboard.css';
 import Game from '../../components/game/Game.js';
 import Sidebar from '../../components/sidebar/Sidebar.js';
+import Lobby from '../../components/lobby/Lobby.js';
 import { troomp, dhonu, billnbob } from '../../characterObjects/characters.js';
 import teststage from '../../stageConfigs/teststage.js';
 import defaultControls from '../../controlConfigs/default.js';
@@ -10,7 +11,11 @@ import wasdControls from '../../controlConfigs/wasd.js';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      gameStart: false,
+      username: '',
+      token: ''
+    }
   }
 
   // check login
@@ -21,18 +26,30 @@ class Dashboard extends Component {
     if (!token || !username) {
       this.props.history.push('/login');
       window.alert('logged out');
+    } else {
+      this.setState({
+        username,
+        token
+      });
     }
 
     console.log(this.context);
   }
 
   render() {
+    var { gameStart, username, token } = this.state;
+
     return (
       <div className="Dashboard">
         <div className="header-placeholder">PixelSmash</div>
         <div className="main-row">
           <div className="left-side">
-            <Game playerConfigs={[billnbob, dhonu]} stageConfig={teststage} controlConfigs={[wasdControls, defaultControls]} socketContext={this.context} />
+            {!gameStart &&
+              <Lobby loggedInUser={username} loggedInToken={token} />
+            }
+            {gameStart &&
+              <Game playerConfigs={[billnbob, dhonu]} stageConfig={teststage} controlConfigs={[wasdControls, defaultControls]} socketContext={this.context} />
+            }
           </div>
           <div className="right-side">
             <Sidebar />
