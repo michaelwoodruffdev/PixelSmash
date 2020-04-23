6a976d7229c1e17ed9c99e7cf87506ce93a6ef3c
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-04-23 12:53:06 -0500

    implement improved lobby-flow (frontend)

--- frontend/src/layouts/dashboard/Dashboard.js
+++ frontend/src/layouts/dashboard/Dashboard.js
@@ -13,18 +13,24 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isHost: true,
+      host: localStorage.getItem("__pixelsmash__username"),
+      guest: 'empty slot',
+      lobbyNo: null,
       gameStart: false,
-      username: '',
       token: '', 
-      isHost: true, 
-      guest: '', 
-      lobbyNo: null
+      gameLobby: null, 
+      characterMap: {}, 
+      selectedCharacters: []
     }
 
     this.onGetUserInfo = this.onGetUserInfo.bind(this);
     this.onInvitationHeard = this.onInvitationHeard.bind(this);
     this.onInviteFriendHeard = this.onInviteFriendHeard.bind(this);
     this.onStartGameHeard = this.onStartGameHeard.bind(this);
+    this.onGuestJoined = this.onGuestJoined.bind(this);
+    this.onJoinHost = this.onJoinHost.bind(this);
+    this.onGameReadyToStart = this.onGameReadyToStart.bind(this);
   }
 
   // check login
@@ -37,16 +43,24 @@ class Dashboard extends Component {
       window.alert('logged out');
     } else {
       this.setState({
-        username,
         token
       });
     }
+
+    let characterMap = this.state.characterMap;
+    characterMap['dhonu'] = dhonu;
+    characterMap['billnbob'] = billnbob;
+    this.setState({
+      characterMap: characterMap
+    });
   }
 
+  // sets up socket on backend
   onGetUserInfo() {
-    this.context.emit("getUserInfoHeard", this.state.username);
+    this.context.emit("getUserInfoHeard", localStorage.getItem("__pixelsmash__username"));
   }
 
+  // listens for successful or failed invite
   onInviteFriendHeard(success) {
     if (success) {
       window.alert("invite sent");
@@ -55,6 +69,7 @@ class Dashboard extends Component {
     }
   }
 
+  // on reception of invite
   onInvitationHeard(sender) {
     let answer = window.confirm(`${sender} invited you to a game`);
     if (answer) {
@@ -62,17 +77,53 @@ class Dashboard extends Component {
     }
   }
 
-  onStartGameHeard(lobbyNo) {
+  // reacts to join event
+  onGuestJoined(guest, lobbyNo) {
     this.setState({
-      lobbyNo: lobbyNo
+      guest: guest,
+      isHost: true, 
+      lobbyNo
     });
+  }
+
+  // reacts to join event
+  onJoinHost(host, lobbyNo) {
+    this.setState({
+      guest: localStorage.getItem("__pixelsmash__username"),
+      isHost: false,
+      host: host, 
+      lobbyNo
+    });
+  }
+
+  onStartGameHeard(lobbyNo) {
     this.setState({
       gameStart: true
     })
   }
 
+  onGameReadyToStart(playerKey, hostFighter, guestFighter, hostFighterKey, guestFighterKey) {
+    this.setState({
+      playerKey: playerKey, 
+      hostFighterKey: hostFighterKey, 
+      guestFighter, guestFighterKey
+    });
+    let selectedCharacters = this.state.selectedCharacters;
+    selectedCharacters[0] = this.state.characterMap[`${hostFighter}`];
+    selectedCharacters[1] = this.state.characterMap[`${guestFighter}`];
+    this.setState({
+      selectedCharacters: selectedCharacters
+    });
+
+    console.log('about to set gamestart to true');
+
+    this.setState({
+      gameStart: true
+    });
+  }
+
   render() {
-    var { gameStart, username, token, lobbyNo } = this.state;
+    var { gameStart, username, token, lobbyNo, host, guest, isHost, playerKey, selectedCharacters, hostFighterKey, guestFighterKey } = this.state;
 
     return (
       <div className="Dashboard">
@@ -80,20 +131,23 @@ class Dashboard extends Component {
         <div className="main-row">
           <div className="left-side">
             {!gameStart &&
-              <Lobby loggedInUser={username} loggedInToken={token} socketContext={this.context}/>
+              <Lobby loggedInUser={username} loggedInToken={token} socketContext={this.context} host={host} guest={guest} isHost={isHost}/>
             }
             {gameStart &&
-              <Game playerConfigs={[billnbob, dhonu]} stageConfig={teststage} controlConfigs={[wasdControls, defaultControls]} socketContext={this.context} lobbyNo={lobbyNo}/>
+              <Game playerConfigs={selectedCharacters} stageConfig={teststage} controlConfigs={[wasdControls]} socketContext={this.context} lobbyNo={lobbyNo} playerKey={playerKey} hostFighterKey={hostFighterKey} guestFighterKey={guestFighterKey} host={host} guest={guest}/>
             }
           </div>
           <div className="right-side">
-            <Sidebar socketContext={this.context}/>
+            <Sidebar socketContext={this.context} />
           </div>
         </div>
         <Event event="getUserInfo" handler={this.onGetUserInfo} />
         <Event event="inviteFriendHeard" handler={this.onInviteFriendHeard} />
         <Event event="invitationHeard" handler={this.onInvitationHeard} />
         <Event event="startGameHeard" handler={this.onStartGameHeard} />
+        <Event event="joinHost" handler={this.onJoinHost} />
+        <Event event="guestJoined" handler={this.onGuestJoined} />
+        <Event event="gameReadyToStart" handler={this.onGameReadyToStart} />
       </div>
     );
   }
