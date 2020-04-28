96bfd1506a2fcae27cb810f763d1e5fad79c5ee9
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-04-28 13:12:34 -0500

    add support for several dhonu skins

--- frontend/src/layouts/dashboard/Dashboard.js
+++ frontend/src/layouts/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import './Dashboard.css';
 import Game from '../../components/game/Game.js';
 import Sidebar from '../../components/sidebar/Sidebar.js';
 import Lobby from '../../components/lobby/Lobby.js';
-import { troomp, dhonu, billnbob } from '../../characterObjects/characters.js';
+import { troomp, dhonu, billnbob, dhonu_dark, dhonu_pink, dhonu_gold } from '../../characterObjects/characters.js';
 import teststage from '../../stageConfigs/himalopolis.js';
 import defaultControls from '../../controlConfigs/default.js';
 import wasdControls from '../../controlConfigs/wasd.js';
@@ -52,6 +52,9 @@ class Dashboard extends Component {
     let characterMap = this.state.characterMap;
     characterMap['dhonu'] = dhonu;
     characterMap['billnbob'] = billnbob;
+    characterMap['dhonu_dark'] = dhonu_dark;
+    characterMap['dhonu_pink'] = dhonu_pink;
+    characterMap['dhonu_gold'] = dhonu_gold;
     this.setState({
       characterMap: characterMap
     });
